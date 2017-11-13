cb3dd9dc6afd3505c583d6de8359f34e2dc413f5
Ivan Barreto <iabarreto11@gmail.com>
2017-11-12 21:19:04 -0700

    Add some basic routes and views

---
+++ app.rb
@@ -0,0 +1,24 @@
+require 'sinatra'
+
+projects = [
+  { :id => 0, :name => 'Test 1', :description => 'description 1' },
+  { :id => 1, :name => 'Test 2', :description => 'description 2' }
+]
+
+get '/' do
+  erb :index
+end
+
+get '/projects' do
+  erb :projects, :locals => { :projects => projects }
+end
+
+post '/projects' do
+  projects << { :name => params[:project_name], :description => params[:project_description] }
+  redirect('/projects')
+end
+
+get '/projects/:project_id' do
+  project = projects[params[:project_id].to_i]
+  erb :project, :locals => { :project => project }
+end
