1e4948225a1f0793cfec5add08fa20fd0acd3424
Ivan Barreto <iabarreto11@gmail.com>
2017-11-16 19:41:49 -0700

    integrate with Github API to display repos

--- app.rb
+++ app.rb
@@ -1,4 +1,6 @@
 require 'sinatra'
+require 'net/http'
+require 'json'
 
 projects = [
   { :id => 0, :name => 'Test 1', :description => 'description 1' },
@@ -17,4 +19,13 @@ end
 get '/projects/:project_id' do
   project = projects[params[:project_id].to_i]
   erb :project, :locals => { :project => project }
+end
+
+get '/repos' do
+  access_token = ENV['ACCESS_TOKEN']
+  response = Net::HTTP.get_response(URI("https://api.github.com/users/ibarreto/repos?acces_token=#{access_token}"))
+  repos = JSON.parse(response.body)
+  repos.map!{|repo| {:name => repo["name"], :description => repo["description"]}}
+  puts repos
+  erb :repos, :locals => { :repos => repos }
 end
