314893215f923b2a0d3b2d2c0abbee400c7d48d0
Ivan Barreto <iabarreto11@gmail.com>
2017-11-16 20:46:25 -0700

    use github api to create new repos

--- app.rb
+++ app.rb
@@ -9,16 +9,28 @@ projects = [
 
 get '/' do
   access_token = ENV['ACCESS_TOKEN']
-  response = Net::HTTP.get_response(URI("https://api.github.com/users/ibarreto/repos?acces_token=#{access_token}"))
+  response = Net::HTTP.get_response(URI("https://api.github.com/users/ibarreto/repos?access_token=#{access_token}"))
   repos = JSON.parse(response.body)
   repos.map!{|repo| {:id => repo["id"], :name => repo["name"], :description => repo["description"]}}
 
-  projects += repos
-  erb :projects, :locals => { :projects => projects }
+  erb :projects, :locals => { :projects => repos }
 end
 
 post '/projects' do
-  projects << { :name => params[:project_name], :description => params[:project_description], :id => projects.size }
+  access_token = ENV['ACCESS_TOKEN']
+  project_data = {:name => params[:project_name], :description => params[:project_description]}
+
+  uri = URI("https://api.github.com/user/repos")
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+  request = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
+  request.body = project_data.to_json
+  request['Authorization'] = "token #{access_token}"
+
+  response = http.request(request)
+
   redirect('/')
 end
 
