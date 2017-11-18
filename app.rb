568f9cb607cd2077dfbabbc59bcb3ecd31f765e2
Ivan Barreto <iabarreto11@gmail.com>
2017-11-17 20:49:32 -0700

    slightly refactor code

--- app.rb
+++ app.rb
@@ -2,9 +2,12 @@ require 'sinatra'
 require 'net/http'
 require 'json'
 
+GITHUB_API_URL = 'https://api.github.com'
+ACCESS_TOKEN = ENV['ACCESS_TOKEN']
+GITHUB_USERNAME = ENV['GITHUB_USERNAME']
+
 get '/' do
-  access_token = ENV['ACCESS_TOKEN']
-  response = Net::HTTP.get_response(URI("https://api.github.com/users/ibarreto/repos?access_token=#{access_token}"))
+  response = Net::HTTP.get_response(URI("#{GITHUB_API_URL}/users/#{GITHUB_USERNAME}/repos?access_token=#{ACCESS_TOKEN}"))
   repos = JSON.parse(response.body)
   repos.map!{|repo| {:id => repo["id"], :name => repo["name"], :description => repo["description"]}}
 
@@ -12,17 +15,16 @@ get '/' do
 end
 
 post '/projects' do
-  access_token = ENV['ACCESS_TOKEN']
   project_data = {:name => params[:project_name], :description => params[:project_description]}
 
-  uri = URI("https://api.github.com/user/repos")
+  uri = URI("#{GITHUB_API_URL}/user/repos")
   http = Net::HTTP.new(uri.host, uri.port)
   http.use_ssl = true
   http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
   request = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
   request.body = project_data.to_json
-  request['Authorization'] = "token #{access_token}"
+  request['Authorization'] = "token #{ACCESS_TOKEN}"
 
   response = http.request(request)
 
@@ -30,8 +32,7 @@ post '/projects' do
 end
 
 get '/projects/:project_name' do
-  access_token = ENV['ACCESS_TOKEN']
-  response = Net::HTTP.get_response(URI("https://api.github.com/repos/ibarreto/#{params[:project_name]}?access_token=#{access_token}"))
+  response = Net::HTTP.get_response(URI("#{GITHUB_API_URL}/repos/#{GITHUB_USERNAME}/#{params[:project_name]}?access_token=#{ACCESS_TOKEN}"))
   repo = JSON.parse(response.body)
   project = {:name => repo["name"], :description => repo["description"]}
 
